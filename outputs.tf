76ea3490f4665d1cff9adf3be6d77fd399541eb7
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:32:47 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "windows_function_app_slots_id" {
+  description = "Map of id values across all windows_function_app_slots, keyed the same as var.windows_function_app_slots"
+  value       = { for k, v in azurerm_windows_function_app_slot.windows_function_app_slots : k => v.id }
+}
 output "windows_function_app_slots_app_settings" {
   description = "Map of app_settings values across all windows_function_app_slots, keyed the same as var.windows_function_app_slots"
   value       = { for k, v in azurerm_windows_function_app_slot.windows_function_app_slots : k => v.app_settings }
