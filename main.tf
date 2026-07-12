3ae8942f7d912865fbb9d4e5fdc32db704c105f8
Daniel Allen <dan@aeternagroup.net>
2026-07-12 13:21:26 +0100

    Update to schema version v4.80.0

--- main.tf
+++ main.tf
@@ -68,7 +68,7 @@ resource "azurerm_windows_function_app_slot" "windows_function_app_slots" {
     health_check_path                 = each.value.site_config.health_check_path
     http2_enabled                     = each.value.site_config.http2_enabled
     dynamic "ip_restriction" {
-      for_each = each.value.site_config.ip_restriction != null ? [each.value.site_config.ip_restriction] : []
+      for_each = each.value.site_config.ip_restriction != null ? each.value.site_config.ip_restriction : []
       content {
         action      = ip_restriction.value.action
         description = ip_restriction.value.description
@@ -98,7 +98,7 @@ resource "azurerm_windows_function_app_slot" "windows_function_app_slots" {
     remote_debugging_version         = each.value.site_config.remote_debugging_version
     runtime_scale_monitoring_enabled = each.value.site_config.runtime_scale_monitoring_enabled
     dynamic "scm_ip_restriction" {
-      for_each = each.value.site_config.scm_ip_restriction != null ? [each.value.site_config.scm_ip_restriction] : []
+      for_each = each.value.site_config.scm_ip_restriction != null ? each.value.site_config.scm_ip_restriction : []
       content {
         action      = scm_ip_restriction.value.action
         description = scm_ip_restriction.value.description
@@ -231,7 +231,7 @@ resource "azurerm_windows_function_app_slot" "windows_function_app_slots" {
       }
       config_file_path = auth_settings_v2.value.config_file_path
       dynamic "custom_oidc_v2" {
-        for_each = auth_settings_v2.value.custom_oidc_v2 != null ? [auth_settings_v2.value.custom_oidc_v2] : []
+        for_each = auth_settings_v2.value.custom_oidc_v2 != null ? auth_settings_v2.value.custom_oidc_v2 : []
         content {
           client_id                     = custom_oidc_v2.value.client_id
           name                          = custom_oidc_v2.value.name
@@ -325,7 +325,7 @@ resource "azurerm_windows_function_app_slot" "windows_function_app_slots" {
   }
 
   dynamic "connection_string" {
-    for_each = each.value.connection_string != null ? [each.value.connection_string] : []
+    for_each = each.value.connection_string != null ? each.value.connection_string : []
     content {
       name  = connection_string.value.name
       type  = connection_string.value.type
@@ -342,7 +342,7 @@ resource "azurerm_windows_function_app_slot" "windows_function_app_slots" {
   }
 
   dynamic "storage_account" {
-    for_each = each.value.storage_account != null ? [each.value.storage_account] : []
+    for_each = each.value.storage_account != null ? each.value.storage_account : []
     content {
       access_key   = storage_account.value.access_key
       account_name = storage_account.value.account_name
--- variables.tf
+++ variables.tf
@@ -291,7 +291,7 @@ EOT
       health_check_eviction_time_in_min = optional(number) # Default: 0
       health_check_path                 = optional(string)
       http2_enabled                     = optional(bool) # Default: false
-      ip_restriction = optional(object({
+      ip_restriction = optional(list(object({
         action      = optional(string) # Default: "Allow"
         description = optional(string)
         headers = optional(object({
@@ -305,7 +305,7 @@ EOT
         priority                  = optional(number) # Default: 65000
         service_tag               = optional(string)
         virtual_network_subnet_id = optional(string)
-      }))
+      })))
       ip_restriction_default_action    = optional(string) # Default: "Allow"
       load_balancing_mode              = optional(string) # Default: "LeastRequests"
       managed_pipeline_mode            = optional(string) # Default: "Integrated"
@@ -315,7 +315,7 @@ EOT
       remote_debugging_enabled         = optional(bool) # Default: false
       remote_debugging_version         = optional(string)
       runtime_scale_monitoring_enabled = optional(bool)
-      scm_ip_restriction = optional(object({
+      scm_ip_restriction = optional(list(object({
         action      = optional(string) # Default: "Allow"
         description = optional(string)
         headers = optional(object({
@@ -329,7 +329,7 @@ EOT
         priority                  = optional(number) # Default: 65000
         service_tag               = optional(string)
         virtual_network_subnet_id = optional(string)
-      }))
+      })))
       scm_ip_restriction_default_action = optional(string) # Default: "Allow"
       scm_minimum_tls_version           = optional(string) # Default: "1.2"
       scm_use_main_ip_restriction       = optional(bool)   # Default: false
@@ -408,13 +408,13 @@ EOT
         client_id = string
       }))
       config_file_path = optional(string)
-      custom_oidc_v2 = optional(object({
+      custom_oidc_v2 = optional(list(object({
         client_id                     = string
         name                          = string
         name_claim_type               = optional(string)
         openid_configuration_endpoint = string
         scopes                        = optional(list(string))
-      }))
+      })))
       default_provider = optional(string)
       excluded_paths   = optional(list(string))
       facebook_v2 = optional(object({
@@ -478,23 +478,23 @@ EOT
       })
       storage_account_url = string
     }))
-    connection_string = optional(object({
+    connection_string = optional(list(object({
       name  = string
       type  = string
       value = string
-    }))
+    })))
     identity = optional(object({
       identity_ids = optional(set(string))
       type         = string
     }))
-    storage_account = optional(object({
+    storage_account = optional(list(object({
       access_key   = string
       account_name = string
       mount_path   = optional(string)
       name         = string
       share_name   = string
       type         = string
-    }))
+    })))
   }))
 }
 
