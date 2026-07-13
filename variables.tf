29649158f6e215ea9ab731552e7f3a0e0ae0b676
Daniel Allen <dan@aeternagroup.net>
2026-07-13 19:40:00 +0100

    Update to schema version v4.80.0

--- variables.tf
+++ variables.tf
@@ -240,30 +240,30 @@ EOT
     function_app_id                                  = string
     name                                             = string
     virtual_network_subnet_id                        = optional(string)
-    virtual_network_backup_restore_enabled           = optional(bool) # Default: false
+    virtual_network_backup_restore_enabled           = optional(bool)
     tags                                             = optional(map(string))
-    storage_uses_managed_identity                    = optional(bool) # Default: false
+    storage_uses_managed_identity                    = optional(bool)
     storage_key_vault_secret_id                      = optional(string)
     storage_account_name                             = optional(string)
     storage_account_access_key                       = optional(string)
     storage_account_access_key_key_vault_id          = optional(string)
     storage_account_access_key_key_vault_secret_name = optional(string)
     service_plan_id                                  = optional(string)
-    public_network_access_enabled                    = optional(bool) # Default: true
+    public_network_access_enabled                    = optional(bool)
     key_vault_reference_identity_id                  = optional(string)
-    https_only                                       = optional(bool)   # Default: false
-    functions_extension_version                      = optional(string) # Default: "~4"
-    ftp_publish_basic_authentication_enabled         = optional(bool)   # Default: true
-    enabled                                          = optional(bool)   # Default: true
-    daily_memory_time_quota                          = optional(number) # Default: 0
-    content_share_force_disabled                     = optional(bool)   # Default: false
-    client_certificate_mode                          = optional(string) # Default: "Optional"
+    https_only                                       = optional(bool)
+    functions_extension_version                      = optional(string)
+    ftp_publish_basic_authentication_enabled         = optional(bool)
+    enabled                                          = optional(bool)
+    daily_memory_time_quota                          = optional(number)
+    content_share_force_disabled                     = optional(bool)
+    client_certificate_mode                          = optional(string)
     client_certificate_exclusion_paths               = optional(string)
-    client_certificate_enabled                       = optional(bool) # Default: false
-    builtin_logging_enabled                          = optional(bool) # Default: true
+    client_certificate_enabled                       = optional(bool)
+    builtin_logging_enabled                          = optional(bool)
     app_settings                                     = optional(map(string))
-    vnet_image_pull_enabled                          = optional(bool) # Default: false
-    webdeploy_publish_basic_authentication_enabled   = optional(bool) # Default: true
+    vnet_image_pull_enabled                          = optional(bool)
+    webdeploy_publish_basic_authentication_enabled   = optional(bool)
     site_config = object({
       always_on             = optional(bool)
       api_definition_url    = optional(string)
@@ -271,13 +271,13 @@ EOT
       app_command_line      = optional(string)
       app_scale_limit       = optional(number)
       app_service_logs = optional(object({
-        disk_quota_mb         = optional(number) # Default: 35
+        disk_quota_mb         = optional(number)
         retention_period_days = optional(number)
       }))
       application_insights_connection_string = optional(string)
       application_insights_key               = optional(string)
       application_stack = optional(object({
-        dotnet_version              = optional(string) # Default: "v4.0"
+        dotnet_version              = optional(string)
         java_version                = optional(string)
         node_version                = optional(string)
         powershell_core_version     = optional(string)
@@ -287,59 +287,59 @@ EOT
       auto_swap_slot_name = optional(string)
       cors = optional(object({
         allowed_origins     = optional(set(string))
-        support_credentials = optional(bool) # Default: false
+        support_credentials = optional(bool)
       }))
       default_documents                 = optional(list(string))
       elastic_instance_minimum          = optional(number)
-      ftps_state                        = optional(string) # Default: "Disabled"
-      health_check_eviction_time_in_min = optional(number) # Default: 0
+      ftps_state                        = optional(string)
+      health_check_eviction_time_in_min = optional(number)
       health_check_path                 = optional(string)
-      http2_enabled                     = optional(bool) # Default: false
+      http2_enabled                     = optional(bool)
       ip_restriction = optional(list(object({
-        action      = optional(string) # Default: "Allow"
+        action      = optional(string)
         description = optional(string)
-        headers = optional(object({
+        headers = optional(list(object({
           x_azure_fdid      = optional(list(string))
           x_fd_health_probe = optional(list(string))
           x_forwarded_for   = optional(list(string))
           x_forwarded_host  = optional(list(string))
-        }))
+        })))
         ip_address                = optional(string)
         name                      = optional(string)
-        priority                  = optional(number) # Default: 65000
+        priority                  = optional(number)
         service_tag               = optional(string)
         virtual_network_subnet_id = optional(string)
       })))
-      ip_restriction_default_action    = optional(string) # Default: "Allow"
-      load_balancing_mode              = optional(string) # Default: "LeastRequests"
-      managed_pipeline_mode            = optional(string) # Default: "Integrated"
+      ip_restriction_default_action    = optional(string)
+      load_balancing_mode              = optional(string)
+      managed_pipeline_mode            = optional(string)
       minimum_tls_cipher_suite         = optional(string)
-      minimum_tls_version              = optional(string) # Default: "1.2"
+      minimum_tls_version              = optional(string)
       pre_warmed_instance_count        = optional(number)
-      remote_debugging_enabled         = optional(bool) # Default: false
+      remote_debugging_enabled         = optional(bool)
       remote_debugging_version         = optional(string)
       runtime_scale_monitoring_enabled = optional(bool)
       scm_ip_restriction = optional(list(object({
-        action      = optional(string) # Default: "Allow"
+        action      = optional(string)
         description = optional(string)
-        headers = optional(object({
+        headers = optional(list(object({
           x_azure_fdid      = optional(list(string))
           x_fd_health_probe = optional(list(string))
           x_forwarded_for   = optional(list(string))
           x_forwarded_host  = optional(list(string))
-        }))
+        })))
         ip_address                = optional(string)
         name                      = optional(string)
-        priority                  = optional(number) # Default: 65000
+        priority                  = optional(number)
         service_tag               = optional(string)
         virtual_network_subnet_id = optional(string)
       })))
-      scm_ip_restriction_default_action = optional(string) # Default: "Allow"
-      scm_minimum_tls_version           = optional(string) # Default: "1.2"
-      scm_use_main_ip_restriction       = optional(bool)   # Default: false
-      use_32_bit_worker                 = optional(bool)   # Default: true
-      vnet_route_all_enabled            = optional(bool)   # Default: false
-      websockets_enabled                = optional(bool)   # Default: false
+      scm_ip_restriction_default_action = optional(string)
+      scm_minimum_tls_version           = optional(string)
+      scm_use_main_ip_restriction       = optional(bool)
+      use_32_bit_worker                 = optional(bool)
+      vnet_route_all_enabled            = optional(bool)
+      websockets_enabled                = optional(bool)
       worker_count                      = optional(number)
     })
     auth_settings = optional(object({
@@ -379,8 +379,8 @@ EOT
         oauth_scopes               = optional(list(string))
       }))
       runtime_version               = optional(string)
-      token_refresh_extension_hours = optional(number) # Default: 72
-      token_store_enabled           = optional(bool)   # Default: false
+      token_refresh_extension_hours = optional(number)
+      token_store_enabled           = optional(bool)
       twitter = optional(object({
         consumer_key                 = string
         consumer_secret              = optional(string)
@@ -427,7 +427,7 @@ EOT
         graph_api_version       = optional(string)
         login_scopes            = optional(list(string))
       }))
-      forward_proxy_convention                = optional(string) # Default: "NoProxy"
+      forward_proxy_convention                = optional(string)
       forward_proxy_custom_host_header_name   = optional(string)
       forward_proxy_custom_scheme_header_name = optional(string)
       github_v2 = optional(object({
@@ -441,19 +441,19 @@ EOT
         client_secret_setting_name = string
         login_scopes               = optional(list(string))
       }))
-      http_route_api_prefix = optional(string) # Default: "/.auth"
+      http_route_api_prefix = optional(string)
       login = object({
         allowed_external_redirect_urls    = optional(list(string))
-        cookie_expiration_convention      = optional(string) # Default: "FixedTime"
-        cookie_expiration_time            = optional(string) # Default: "08:00:00"
+        cookie_expiration_convention      = optional(string)
+        cookie_expiration_time            = optional(string)
         logout_endpoint                   = optional(string)
-        nonce_expiration_time             = optional(string) # Default: "00:05:00"
-        preserve_url_fragments_for_logins = optional(bool)   # Default: false
-        token_refresh_extension_time      = optional(number) # Default: 72
-        token_store_enabled               = optional(bool)   # Default: false
+        nonce_expiration_time             = optional(string)
+        preserve_url_fragments_for_logins = optional(bool)
+        token_refresh_extension_time      = optional(number)
+        token_store_enabled               = optional(bool)
         token_store_path                  = optional(string)
         token_store_sas_setting_name      = optional(string)
-        validate_nonce                    = optional(bool) # Default: true
+        validate_nonce                    = optional(bool)
       })
       microsoft_v2 = optional(object({
         allowed_audiences          = optional(list(string))
@@ -462,22 +462,22 @@ EOT
         login_scopes               = optional(list(string))
       }))
       require_authentication = optional(bool)
-      require_https          = optional(bool)   # Default: true
-      runtime_version        = optional(string) # Default: "~1"
+      require_https          = optional(bool)
+      runtime_version        = optional(string)
       twitter_v2 = optional(object({
         consumer_key                 = string
         consumer_secret_setting_name = string
       }))
-      unauthenticated_action = optional(string) # Default: "RedirectToLoginPage"
+      unauthenticated_action = optional(string)
     }))
     backup = optional(object({
-      enabled = optional(bool) # Default: true
+      enabled = optional(bool)
       name    = string
       schedule = object({
         frequency_interval       = number
         frequency_unit           = string
-        keep_at_least_one_backup = optional(bool)   # Default: false
-        retention_period_days    = optional(number) # Default: 30
+        keep_at_least_one_backup = optional(bool)
+        retention_period_days    = optional(number)
         start_time               = optional(string)
       })
       storage_account_url = string
